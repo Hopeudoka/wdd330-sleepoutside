f5e38d0a8d0c4a544b766446e4239d5bd766736f
Chrysologue <ChrysoBeatz@gmail.com>
2025-05-17 21:40:28 +0300

    Full code fix

--- src/js/product.js
+++ src/js/product.js
@@ -1,24 +1,10 @@
-import { getLocalStorage, setLocalStorage, getParam } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetail from "./ProductDetails.mjs";
 
-const dataSource = new ProductData("tents");
 const productId = getParam("product");
-console.log(dataSource.findProductById(productId));
+const dataSource = new ProductData("tents");
+const productDetail = ProductDetail(productId, dataSource);
 
-function addProductToCart(product) {
-  const cartItems = getLocalStorage("so-cart") || [];
-  cartItems.push(product);
-  setLocalStorage("so-cart", cartItems);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
+productDetail.init();
 
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
-
-  
